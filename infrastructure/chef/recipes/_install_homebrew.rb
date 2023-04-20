dfb87b27ae150948c7495ede17e6dbea6b1ef7dc
Jose Luis Jimenez <jose.luis@sketch.com>
2023-04-20 11:26:19 +0200

    chore: Update first run

--- infrastructure/chef/recipes/_install_homebrew.rb
+++ infrastructure/chef/recipes/_install_homebrew.rb
@@ -5,6 +5,10 @@ execute 'Install homebrew' do
   not_if { ::File.exist?("#{homebrew_prefix}/bin/brew") }
 end
 
+# Those 2 script must be executed in order to have brew on the path
+#(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/jenkins/.zprofile
+#eval "$(/opt/homebrew/bin/brew shellenv)"
+
 # This is needed for the homebrew_package resource to work.
 # It's used to check for the brew executable owner in order to run as that user
 ENV['PATH'] = "#{homebrew_prefix}/bin:#{ENV['PATH']}"
