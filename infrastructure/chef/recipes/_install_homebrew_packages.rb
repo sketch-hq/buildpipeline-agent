38879c63c39d1e358a88c43913aa9fbfeef6a460
Jose Luis Jimenez <jose.luis@sketch.com>
2024-03-01 10:50:13 +0100

    chore: Update version & add check-jsonschema

--- infrastructure/chef/recipes/_install_homebrew_packages.rb
+++ infrastructure/chef/recipes/_install_homebrew_packages.rb
@@ -10,5 +10,4 @@ homebrew_package('php') { action :upgrade }
 homebrew_package('fastlane') { version "2.212.2" }
 homebrew_package('jq') { action :upgrade }
 homebrew_package('xcbeautify') { action :upgrade }
-
-
+homebrew_package('check-jsonschema') { action :upgrade }
--- infrastructure/chef/recipes/_select_xcode.rb
+++ infrastructure/chef/recipes/_select_xcode.rb
@@ -6,7 +6,7 @@ execute "Select Xcode #{xcode_selected_version}" do
 end
 
 execute "Symlink bo python binary inside Xcode #{xcode_selected_version}" do
-  command "bo symlink /Applications/Xcode-#{xcode_selected_version}.app/Contents/Developer/usr/bin/python3"
+  command "/usr/local/bin/bo symlink /Applications/Xcode-#{xcode_selected_version}.app/Contents/Developer/usr/bin/python3"
   only_if { ::Dir.exists?("/Applications/Xcode-#{xcode_selected_version}.app") }
 end
 
