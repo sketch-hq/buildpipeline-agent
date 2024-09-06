7e1386427888cc60ad8b80d89f82755050de0fbd
Jose Luis Jimenez <jose.luis@sketch.com>
2024-09-06 09:49:27 +0200

    feat: Add tailscale

--- infrastructure/chef/metadata.rb
+++ infrastructure/chef/metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Control Center'
 maintainer_email 'control-center@sketch.com'
 license 'All Rights Reserved'
 description 'Installs/Configures a Build Pipeline Agent'
-version '0.1.114'
+version '0.1.118'
 chef_version '>= 16.0'
 
 supports 'mac_os_x'
