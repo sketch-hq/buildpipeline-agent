6ca12b1d31e353cc7a34be6149856d322b131d2c
Jose Luis Jimenez <jose.luis@sketch.com>
2024-11-18 11:36:44 +0100

    chore: Prevent running some brew-installs on J13 -old machine-

--- infrastructure/chef/recipes/_install_homebrew_packages.rb
+++ infrastructure/chef/recipes/_install_homebrew_packages.rb
@@ -13,6 +13,7 @@ end
 homebrew_package('coreutils') { action :upgrade }
 homebrew_package('tree') { action :upgrade }
 homebrew_package('gawk') { action :upgrade }
+homebrew_package('jq') { action :upgrade }
 
 # Upgrade PHP only if macOS version is at least 13.0
 if macos_version_at_least?('13.0')
@@ -21,7 +22,23 @@ else
   Chef::Log.warn("Skipping PHP upgrade: macOS version is below 13.0")
 end
 
-homebrew_package('fastlane') { version "2.225.0" }
-homebrew_package('jq') { action :upgrade }
-homebrew_package('xcbeautify') { action :upgrade }
-homebrew_package('check-jsonschema') { action :upgrade }
+# Install Fastlane only if macOS version is at least 13.0
+if macos_version_at_least?('13.0')
+  homebrew_package('fastlane') { version "2.225.0" }
+else
+  Chef::Log.warn("Skipping Fastlane install: macOS version is below 13.0")
+end
+
+# Install xcbeautify only if macOS version is at least 13.0
+if macos_version_at_least?('13.0')
+  homebrew_package('xcbeautify') { action :upgrade }
+else
+  Chef::Log.warn("Skipping xcbeautify install: macOS version is below 13.0")
+end
+
+# Install check-jsonschema only if macOS version is at least 13.0
+if macos_version_at_least?('13.0')
+  homebrew_package('check-jsonschema') { action :upgrade }
+else
+  Chef::Log.warn("Skipping check-jsonschema install: macOS version is below 13.0")
+end
